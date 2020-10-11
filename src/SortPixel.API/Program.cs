cca655ef9f5763c7adf9ea30ae6004c6fdcf4de5
Krzysztof Juszcze <krzjus@gmail.com>
2020-10-11 20:19:00 +0200

    edit project to API

--- src/SortPixel.API/Program.cs
+++ src/SortPixel.API/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace SortPixel
+namespace SortPixel.API
 {
     public class Program
     {
