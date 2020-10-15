1f5900c78b5d8a3caacb55df07a76d3dee42a902
Krzysztof Juszcze <krzjus@gmail.com>
2020-10-15 21:13:46 +0200

    update sort folder

---
+++ src/SortPixel.Services/SortServices/SortStrategies/SelectionSort.cs
@@ -0,0 +1,49 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SortPixel.Services.SortServices
+{
+    public class SelectionSort : SortStrategyAbstract
+    {
+        public override Task<byte[]> Sort(Stream stream)
+        {
+            var task = new Task<byte[]>(() =>
+            {
+                byte[] result;
+
+                Bitmap bitmap = new Bitmap(stream);
+                var colorsArray = FlatteningBitmap(bitmap);
+
+                for (int i = 0; i < colorsArray.Length; i++)
+                {
+                    float min = SumRGB(colorsArray[i]);
+                    int minIndex = i;
+                    for (int j = i +1 ; j < colorsArray.Length; j++)
+                    {
+                        var sumRGB = SumRGB(colorsArray[j]);
+                        if (sumRGB <= min)
+                        {
+                            min = sumRGB;
+                            minIndex = j;
+                        }
+                    }
+                    var helper = colorsArray[i];
+                    colorsArray[i] = colorsArray[minIndex];
+                    colorsArray[minIndex] = helper;
+                }
+
+                UnflatteningColorsArray(bitmap, colorsArray);
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    bitmap.Save(ms, bitmap.RawFormat);
+                    result = ms.ToArray();
+                }
+                return result;
+            });
+            task.Start();
+            return task;
+        }        
+    }
+}
---
+++ src/SortPixel.Services/SortServices/SortStrategies/SortStrategyAbstract.cs
@@ -0,0 +1,42 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SortPixel.Services.SortServices
+{
+    public abstract class SortStrategyAbstract : ISortStrategy
+    {
+        public abstract Task<byte[]> Sort(Stream stream);
+
+        protected virtual Color[] FlatteningBitmap(Bitmap bitmap)
+        {
+            var colorsArray = new Color[bitmap.Height * bitmap.Width];
+            var helperIndex = 0;
+            for (int i = 0; i < bitmap.Height; i++)
+            {
+                for (int j = 0; j < bitmap.Width; j++)
+                {
+                    colorsArray[helperIndex] = bitmap.GetPixel(j, i);
+                    helperIndex++;
+                }
+            }
+
+            return colorsArray;
+        }
+
+        protected virtual void UnflatteningColorsArray(Bitmap bitmap, Color[] colorsArray)
+        {
+            var helperIndex = 0;
+            for (int i = 0; i < bitmap.Height; i++)
+            {
+                for (int j = 0; j < bitmap.Width; j++)
+                {
+                    bitmap.SetPixel(j, i, colorsArray[helperIndex]);
+                    helperIndex++;
+                }
+            }
+        }
+
+        protected int SumRGB(Color color) => color.R + color.G + color.B;
+    }
+}
